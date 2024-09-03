3ff0c9eb19a11f5c64ec89581c08c9781e9e0f66
dasha551 <d.alekseevna2004@gmail.com>
2024-09-04 00:33:42 +0400

    Добавлена анимация счетчика цифр на JS

--- js/script.js
+++ js/script.js
@@ -114,3 +114,72 @@ videoIframes.forEach((iframe, index) => {
     });
 });
 // VIDEO END 
+
+
+// ANIMATION COUNTER START 
+var number = document.querySelector('.number1'),
+		numberTop = number.getBoundingClientRect().top,
+		start = +number.innerHTML, end = +number.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval = setInterval(function() {
+					number.innerHTML = ++start;
+					if(start == end) {
+						clearInterval(interval);
+					}
+				}, 5);
+			}
+		});
+
+        var number2 = document.querySelector('.number2'),
+		number2Top = number2.getBoundingClientRect().top,
+		start2 = +number2.innerHTML, end2 = +number2.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number2Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval2 = setInterval(function() {
+					number2.innerHTML = ++start2;
+					if(start2 == end2) {
+						clearInterval(interval2);
+					}
+				}, 5);
+			}
+		});
+
+        var number3 = document.querySelector('.number3'),
+		number1Top = number3.getBoundingClientRect().top,
+		start1 = +number3.innerHTML, end1 = +number3.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number1Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval1 = setInterval(function() {
+					number3.innerHTML = ++start1;
+					if(start1 == end1) {
+						clearInterval(interval1);
+					}
+				}, 7);
+			}
+		});
+
+
+        var number3 = document.querySelector('.number3'),
+		number3Top = number3.getBoundingClientRect().top,
+		start3 = +number3.innerHTML, end3 = +number3.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number3Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval3 = setInterval(function() {
+					number3.innerHTML = ++start3;
+					if(start3 == end3) {
+						clearInterval(interval3);
+					}
+				}, 5);
+			}
+		});
+
+// ANIMATION COUNTER END 
